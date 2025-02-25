1611e6059903e88880cf6e43dc79cade1b08dfc0
Hotto Dogu <ian@takol.tw>
2025-02-25 16:27:28 +0800

    lab00 and lab01

---
+++ FCPS_eUnits/Unit1/src/Lab01.java
@@ -0,0 +1,74 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+
+public class Lab01 {
+
+	
+	public static void main(String[] args) {
+		Display.openWorld("../maps/school.map");
+		Robot a = new Robot();
+      Robot b = new Robot(4,5,3,0);
+      a.move();
+      a.move();
+      a.turnLeft();
+      a.move();
+      a.turnLeft();
+      a.move();
+      a.pickBeeper();
+      a.turnLeft();
+      a.turnLeft();
+      a.move();
+      a.turnLeft();
+      a.turnLeft();
+      a.turnLeft();
+      a.move();
+      a.turnLeft();
+      a.move();
+      a.move();
+      a.move();
+      a.turnLeft();
+      a.move();
+      a.move();
+      a.move();
+      a.turnLeft();
+      a.move();
+      a.move();
+      a.turnLeft();
+      a.turnLeft();
+      a.turnLeft();
+      a.move();
+      a.putBeeper();
+      b.pickBeeper();
+      b.move();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.turnLeft();
+      b.turnLeft();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.turnLeft();
+      b.move();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.turnLeft();
+      b.turnLeft();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.turnLeft();
+      b.move();
+      b.move();
+      b.putBeeper();
+      b.turnLeft();
+      b.turnLeft();
+      b.move();
+	}
+
+}
